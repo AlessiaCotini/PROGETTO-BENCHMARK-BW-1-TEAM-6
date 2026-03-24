79781f599f30d7779082e16a0fed498c5a143ad4
Amir B. Kahna <amir.kahna2@gmail.com>
2026-03-24 11:59:18 +0100

    Added result index

--- script/cake.js
+++ script/cake.js
@@ -1,28 +1,32 @@
+const wrongAnswer = document.getElementById("wrong");
+const correctAnswer = document.getElementById("correct");
 const doughnut = document.getElementById("cakeResult");
-if (doughnut) {
-  let savedScore = Number(localStorage.getItem("score"));
-  let total = Number(localStorage.getItem("total"));
-  let correct = savedScore;
-  let wrong = total - savedScore;
-  new Chart(doughnut, {
-    type: "doughnut",
-    data: {
-      labels: ["Correct", "Wrong"],
-      datasets: [
-        {
-          data: [correct, wrong],
-          backgroundColor: ["#00FFFF", "#C2128D"],
-          borderWidth: 0,
-        },
-      ],
-    },
-    options: {
-      cutout: "70%",
-      plugins: {
-        legend: {
-          display: false,
-        },
+
+let savedScore = Number(localStorage.getItem("score"));
+let total = Number(localStorage.getItem("total"));
+let correct = savedScore;
+let wrong = total - savedScore;
+new Chart(doughnut, {
+  type: "doughnut",
+  data: {
+    labels: ["Correct", "Wrong"],
+    datasets: [
+      {
+        data: [correct, wrong],
+        backgroundColor: ["#00FFFF", "#C2128D"],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    cutout: "70%",
+    plugins: {
+      legend: {
+        display: false,
       },
     },
-  });
-}
+  },
+});
+
+correctAnswer.innerHTML = "<h2>Correct answers</h2>" + " " + correct;
+wrongAnswer.innerHTML = "<h2>Wrong answers</h2>" + " " + wrong;
