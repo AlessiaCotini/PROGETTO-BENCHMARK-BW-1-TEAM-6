2d067cb8a7207d46ed1618e0ac4c1a11426d61ad
AlessiaCotini <alessia.cotini@icloud.com>
2026-03-24 10:29:49 +0100

    commit cake

--- script/quiz.js
+++ script/quiz.js
@@ -1,147 +1,196 @@
-songsId = document.getElementById("songsId");
+const songsId = document.getElementById("songsId");
 let i = 0;
+const buttonQuiz1 = document.createElement("button");
+const buttonQuiz2 = document.createElement("button");
+const buttonQuiz3 = document.createElement("button");
+const questionContainer = document.getElementById("questionContainer");
+const startButton = document.createElement("button");
+const displayTimer = document.getElementById("timerContainer");
+const question = document.createElement("h3");
+let timer = 30;
+let results = [];
+let interval;
+let score = 0;
 const quizArray = [
   {
     name: "bittersweetsymphony",
     src: "../assets/audio/Bitter Sweet Siymphony- The verve.mp3",
-    question: `What Band play this song?`,
+    question: "What Band play this song?",
+    answers: ["THE VERVE", "GAZZELLE", "YUNGBULD"],
     accepted: "THE VERVE",
-    dontacc: "GAZZELLE",
-    tre: "YUNGBULD",
   },
   {
     name: "bloodyvalentine",
     src: "../assets/audio/Bloody Valentine- MGK.mp3",
-    question: `What Band play this song?`,
-    uno: "MACE",
+    question: "What Band play this song?",
+    answers: ["MACE", "MGK", "KASABIAN"],
     accepted: "MGK",
-    dontacc: "KASABIAN",
   },
   {
     name: "californication",
     src: "../assets/audio/Californication- Red Hot Chili Peppers.mp3",
-    question: `What Band play this song?`,
+    question: "What Band play this song?",
+    answers: ["RED HOT CHILI PEPPERS", "AVRIL LAVIGNE", "GODDAMN"],
     accepted: "RED HOT CHILI PEPPERS",
-    dontacc: "AVRIL LAVIGNE",
-    tre: "GODDAMN",
   },
   {
     name: "changes",
     src: "../assets/audio/Changes-2Pac.mp3",
-    question: `What Band play this song?`,
-    dontacc: "LUCIO BATTISTI",
-    due: "NIRVANA",
+    question: "What Band play this song?",
+    answers: ["LUCIO BATTISTI", "NIRVANA", "2PAC"],
     accepted: "2PAC",
   },
   {
     name: "clocks",
     src: "../assets/audio/Clocks-Coldplay.mp3",
-    question: `What Band play this song?`,
-    dontacc: "BEYONCE",
-    due: "LUCIO DALLA",
+    question: "What Band play this song?",
+    answers: ["BEYONCE", "LUCIO DALLA", "COLDPLAY"],
     accepted: "COLDPLAY",
   },
   {
     name: "iloveyou",
     src: "../assets/audio/I Love You-Fontaines DC.mp3",
-    question: `What Band play this song?`,
-    dontacc: "PIXIES",
+    question: "What Band play this song?",
+    answers: ["PIXIES", "FONTAINES D.C.", "GREEN DAY"],
     accepted: "FONTAINES D.C.",
-    tre: "GREEN DAY",
   },
   {
     name: "iris",
     src: "../assets/audio/Iris-Goo Goo Dolls (1).mp3",
-    question: `What Band play this song?`,
+    question: "What Band play this song?",
+    answers: ["GOO GOO DOLLS", "GORILLAZ", "MIA MARTINI"],
     accepted: "GOO GOO DOLLS",
-    dontacc: "GORILLAZ",
-    tre: "MIA MARTINI",
   },
   {
     name: "mywholeworld",
     src: "../assets/audio/My Whole World-GDFRND.mp3",
-    question: `What Band play this song?`,
-    uno: "LAURA PAUSINI",
-    dontacc: "GOOD FRIENDS",
+    question: "What Band play this song?",
+    answers: ["LAURA PAUSINI", "GOOD FRIENDS", "GDFRND"],
     accepted: "GDFRND",
   },
   {
     name: "one",
     src: "../assets/audio/One-Metallica.mp3",
-    question: `What Band play this song?`,
+    question: "What Band play this song?",
+    answers: ["METALLICA", "ROMINA POWER", "JUICE WORLD"],
     accepted: "METALLICA",
-    due: "ROMINA POWER",
-    dontacc: "JUICE WORLD",
   },
   {
     name: "sudinoi",
-    src: "../assets/audio/sudinoi-pupo.mp3",
-    question: `What Band play this song?`,
-    dontepted: "ACDC",
+    src: "../assets/audio/Su Di Noi-Pupo.mp3",
+    question: "What Band play this song?",
+    answers: ["ACDC", "PUPO", "GORILLAZ"],
     accepted: "PUPO",
-    dontacc: "GORILLAZ",
   },
 ];
-const questionContainer = document.getElementById("questionContainer");
 
-const question = document.createElement("h3");
+startButton.innerText = "START GAME";
+startButton.classList.add("startButton");
+questionContainer.appendChild(startButton);
+displayTimer.style.opacity = 0;
+buttonQuiz1.style.opacity = 0;
+buttonQuiz2.style.opacity = 0;
+buttonQuiz3.style.opacity = 0;
+question.style.opacity = 0;
+startButton.addEventListener("click", () => {
+  displayTimer.style.opacity = 1;
+  buttonQuiz1.style.opacity = 1;
+  buttonQuiz2.style.opacity = 1;
+  buttonQuiz3.style.opacity = 1;
+  question.style.opacity = 1;
+  startButton.style.opacity = 0;
+  i = 0;
+  interval = setInterval(() => {
+    displayTimer.innerHTML = timer;
+    timer--;
+
+    if (timer < 0) {
+      timer = 30;
+      i++;
+
+      if (i >= quizArray.length) {
+        endGame();
+        return;
+      }
+
+      songsId.pause();
+      songsId.currentTime = 0;
+
+      songsId.src = quizArray[i].src;
+      songsId.load();
+      songsId.play();
+
+      question.textContent = quizArray[i].question;
+
+      buttonQuiz1.textContent = quizArray[i].answers[0];
+      buttonQuiz2.textContent = quizArray[i].answers[1];
+      buttonQuiz3.textContent = quizArray[i].answers[2];
+    }
+  }, 1000);
+  songsId.src = quizArray[i].src;
+  songsId.load();
+  songsId.play();
+});
 question.textContent = "What Band play this song?";
 questionContainer.appendChild(question);
-const buttonQuiz1 = document.createElement("button");
-const buttonQuiz2 = document.createElement("button");
-const buttonQuiz3 = document.createElement("button");
-
-buttonQuiz1.textContent = Object.values(quizArray[i])[3];
-buttonQuiz2.textContent = Object.values(quizArray[i])[4];
-buttonQuiz3.textContent = Object.values(quizArray[i])[5];
+buttonQuiz1.textContent = quizArray[i].answers[0];
+buttonQuiz2.textContent = quizArray[i].answers[1];
+buttonQuiz3.textContent = quizArray[i].answers[2];
+buttonQuiz1.classList.add("quizButton");
+buttonQuiz2.classList.add("quizButton");
+buttonQuiz3.classList.add("quizButton");
 questionContainer.appendChild(buttonQuiz1);
 questionContainer.appendChild(buttonQuiz2);
 questionContainer.appendChild(buttonQuiz3);
-
 buttonQuiz1.addEventListener("click", function () {
-  checkAnswer(3);
+  checkAnswer(buttonQuiz1.textContent);
 });
 buttonQuiz2.addEventListener("click", function () {
-  checkAnswer(4);
+  checkAnswer(buttonQuiz2.textContent);
 });
 buttonQuiz3.addEventListener("click", function () {
-  checkAnswer(5);
+  checkAnswer(buttonQuiz3.textContent);
 });
-let timer = 30;
-let interval = setInterval(() => {
-  document.getElementById("timerContainer").innerHTML = timer;
-  timer--;
-  if (timer < 0) {
-    timer = 30;
-    i++;
-    songsId.pause();
-    songsId.currentTime = 0;
-
-    songsId.src = quizArray[i].src;
-    buttonQuiz1.textContent = Object.values(quizArray[i])[3];
-    buttonQuiz2.textContent = Object.values(quizArray[i])[4];
-    buttonQuiz3.textContent = Object.values(quizArray[i])[5];
-    songsId.load();
-    songsId.play();
+function checkAnswer(selectedAnswer) {
+  if (selectedAnswer === quizArray[i].accepted) {
+    score++;
+    console.log("Good!", score);
+  } else {
+    console.log("Oh no,try again!");
   }
-}, 1000);
-function checkAnswer(ee) {
+
+  i++;
   timer = 30;
+
+  if (i >= quizArray.length) {
+    endGame();
+    return;
+  }
+
   songsId.pause();
   songsId.currentTime = 0;
+
   songsId.src = quizArray[i].src;
-  i++;
-  buttonQuiz1.textContent = Object.values(quizArray[i])[3];
-  buttonQuiz2.textContent = Object.values(quizArray[i])[4];
-  buttonQuiz3.textContent = Object.values(quizArray[i])[5];
   songsId.load();
   songsId.play();
-  if (Object.keys(quizArray[i])[ee] === "accepted") {
-    return true;
-  } else {
-    return false;
-  }
+
+  question.textContent = quizArray[i].question;
+
+  buttonQuiz1.textContent = quizArray[i].answers[0];
+  buttonQuiz2.textContent = quizArray[i].answers[1];
+  buttonQuiz3.textContent = quizArray[i].answers[2];
 }
-songsId.src = quizArray[i].src;
-songsId.load();
-songsId.play();
+
+function endGame() {
+  songsId.pause();
+  songsId.currentTime = 0;
+  question.style.opacity = 0;
+  buttonQuiz1.style.opacity = 0;
+  buttonQuiz2.style.opacity = 0;
+  buttonQuiz3.style.opacity = 0;
+  displayTimer.style.opacity = 0;
+  const endMessage = document.createElement("h2");
+  questionContainer.appendChild(endMessage);
+  localStorage.setItem("score", score);
+  localStorage.setItem("total", quizArray.length);
+}
