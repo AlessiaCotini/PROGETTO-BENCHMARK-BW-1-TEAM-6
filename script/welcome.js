7f6b427df40cb55f148e93b0f8c2feab6c086841
Saverio Casabianca <Saveriocoppola97@virgilio.it>
2026-03-26 11:45:36 +0100

    fix

--- script/welcome.js
+++ script/welcome.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   if (sessionStorage.getItem("quiz_annullato") === "true") {
     alert("Hai lasciato la pagina del quiz! Il tentativo è stato annullato.");
-    // Importante: rimuovi il segnale così l'alert non compare a ogni refresh
     sessionStorage.removeItem("quiz_annullato");
   }
 };
