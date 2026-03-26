5988d02444dd16537cd6f9f078e8781fc354dcc6
bilal lafdili <bilallafdili23@gmail.com>
2026-03-26 12:17:19 +0100

    modified label page quiz

--- script/credits.js
+++ script/credits.js
@@ -1,12 +1,12 @@
-const images = document.querySelectorAll("img");
+/* const images = document.querySelectorAll("img")
 
 images.forEach((img) => {
   img.addEventListener("mouseenter", () => {
-    img.style.transform = "scale(1.2)";
-    img.style.transition = "transform 0.3s ease";
-  });
+    img.style.transform = "scale(1.5)"
+    img.style.transition = "transform 0.3s ease"
+  })
 
   img.addEventListener("mouseleave", () => {
-    img.style.transform = "scale(1)";
-  });
-});
+    img.style.transform = "scale(1)"
+  })
+}) */
